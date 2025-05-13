e84abc624365635a65010e002d3134300e3ae1ed
Aleksandr <polyukhinas@gmail.com>
2025-05-13 19:35:37 +0200

    Moved from lattice matrix to lattice vec of vec

--- src/common/type.jl
+++ src/common/type.jl
@@ -1,40 +1,40 @@
 using StaticArrays
 
-"""
-3 x 3 matrix type.
+# """
+# 3 x 3 matrix type.
 
-For lattice and recip_lattice.
-"""
-const Mat3{T} = SMatrix{3,3,T,9} where {T}
+# For lattice and recip_lattice.
+# """
+# const Mat3{T} = SMatrix{3,3,T,9} where {T}
 
-"""
-Length-3 vector type.
+# """
+# Length-3 vector type.
 
-For atom posistions, kpoints, etc.
-"""
-const Vec3{T} = SVector{3,T} where {T}
+# For atom posistions, kpoints, etc.
+# """
+# const Vec3{T} = SVector{3,T} where {T}
 
-"""
-`Vector{Vector}` -> `Mat3`
-"""
-Mat3(A::AbstractVector) = Mat3(reduce(hcat, A))
+# """
+# `Vector{Vector}` -> `Mat3`
+# """
+# Mat3(A::AbstractVector) = Mat3(reduce(hcat, A))
 
-"""
-`Mat3` -> `Vec3{Vec3}`
-"""
-Vec3(A::Mat3) = Vec3(eachcol(A))
+# """
+# `Mat3` -> `Vec3{Vec3}`
+# """
+# Vec3(A::Mat3) = Vec3(eachcol(A))
 
-"""
-Pair type associating a `Symbol` with a `Vec3`.
+# """
+# Pair type associating a `Symbol` with a `Vec3`.
 
-For win file `atoms_frac` and `kpoint_path`.
-"""
-const SymbolVec3{T} = Pair{Symbol,Vec3{T}} where {T}
+# For win file `atoms_frac` and `kpoint_path`.
+# """
+# const SymbolVec3{T} = Pair{Symbol,Vec3{T}} where {T}
 
-SymbolVec3(s, v) = SymbolVec3{eltype(v)}(s, v)
-SymbolVec3(s::AbstractString, v) = SymbolVec3(Symbol(s), v)
-SymbolVec3(p::Pair) = SymbolVec3(p.first, p.second)
-SymbolVec3(d::Dict) = SymbolVec3(only(d))
+# SymbolVec3(s, v) = SymbolVec3{eltype(v)}(s, v)
+# SymbolVec3(s::AbstractString, v) = SymbolVec3(Symbol(s), v)
+# SymbolVec3(p::Pair) = SymbolVec3(p.first, p.second)
+# SymbolVec3(d::Dict) = SymbolVec3(only(d))
 
 
 const StrOrSym = Union{AbstractString, Symbol}
