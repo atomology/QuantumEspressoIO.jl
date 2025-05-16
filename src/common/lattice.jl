46c3f7769f1ce6f524dce30dc1d60e255951ca13
Junfeng Qiao <qiaojunfeng@outlook.com>
2025-05-16 10:38:10 +0200

    Add `get_recip_lattice` and `get_lattice`

--- src/common/lattice.jl
+++ src/common/lattice.jl
@@ -3,10 +3,89 @@ using LinearAlgebra
 """
     $(SIGNATURES)
 
+Compute reciprocal-space lattice vectors from real-space lattice vectors.
+
+# Arguments
+- `lattice`: Can be a vector of lattice vectors, or a [`Mat3`](@ref) matrix.
+
+# Examples
+```jldoctest get_recip_lattice; setup = :(using QuantumEspressoIO: get_recip_lattice, mat3)
+lattice = [[0.0, 1.0, 2.0], [3.0, 0.0, 4.0], [5.0, 6.0, 0.0]];
+get_recip_lattice(lattice)
+# output
+3-element Vector{Vector{Float64}}:
+ [-2.6927937030769655, 2.243994752564138, 2.019595277307724]
+ [1.3463968515384828, -1.121997376282069, 0.5609986881410345]
+ [0.4487989505128276, 0.6731984257692414, -0.3365992128846207]
+```
+```jldoctest get_recip_lattice
+lattice = mat3(lattice);
+get_recip_lattice(lattice)
+# output
+3×3 StaticArraysCore.SMatrix{3, 3, Float64, 9} with indices SOneTo(3)×SOneTo(3):
+ -2.69279   1.3464     0.448799
+  2.24399  -1.122      0.673198
+  2.0196    0.560999  -0.336599
+```
+"""
+function get_recip_lattice end
+
+function get_recip_lattice(lattice::Mat3)
+    # In case of Mat3, return a Mat3 as well
+    return 2π * inv(lattice)'
+end
+
+function get_recip_lattice(lattice::AbstractVector{<:AbstractVector})
+    M = reduce(hcat, lattice)
+    recip = 2π * inv(M)'
+    # Return a vector of vectors
+    return collect.(eachcol(recip))
+end
+
+"""
+    $(SIGNATURES)
+
+Compute real-space lattice vectors from reciprocal-space lattice vectors.
+
+# Arguments
+- `recip_lattice`: Can be a vector of reciprocal lattice vectors, or a [`Mat3`](@ref) matrix.
+
+# Examples
+```jldoctest get_lattice; setup = :(using QuantumEspressoIO: get_lattice, mat3, get_recip_lattice)
+recip_lattice = [[0.0, 1.0, 2.0], [3.0, 0.0, 4.0], [5.0, 6.0, 0.0]];
+get_lattice(recip_lattice)
+# output
+3-element Vector{Vector{Float64}}:
+ [-2.6927937030769655, 2.243994752564138, 2.019595277307724]
+ [1.3463968515384828, -1.121997376282069, 0.5609986881410345]
+ [0.4487989505128276, 0.6731984257692414, -0.3365992128846207]
+```
+```jldoctest get_lattice
+recip_lattice = mat3(recip_lattice);
+get_lattice(recip_lattice)
+# output
+3×3 StaticArraysCore.SMatrix{3, 3, Float64, 9} with indices SOneTo(3)×SOneTo(3):
+ -2.69279   1.3464     0.448799
+  2.24399  -1.122      0.673198
+  2.0196    0.560999  -0.336599
+```
+```jldoctest get_lattice
+get_recip_lattice(get_lattice(recip_lattice)) ≈ recip_lattice
+# output
+true
+```
+"""
+const get_lattice = get_recip_lattice
+
+"""
+    $(SIGNATURES)
+
 Convert fractional to Cartesian coordinates based on lattice vectors.
 
 # Arguments
 - `lattice`: Each element is a lattice vector.
+    - For lattice vectors, the unit is usually in angstrom.
+    - For reciprocal lattice vectors, the unit is usually in 1/angstrom.
 - `vec`: a vector or a list of vectors in fractional coordinates.
 
 # Examples
@@ -47,6 +126,8 @@ Convert Cartesian to fractional coordinates based on lattice vectors.
 
 # Arguments
 - `lattice`: Each element is a lattice vector.
+    - For lattice vectors, the unit is usually in angstrom.
+    - For reciprocal lattice vectors, the unit is usually in 1/angstrom.
 - `vec`: a vector or a list of vectors in Cartesian coordinates.
 
 # Examples
