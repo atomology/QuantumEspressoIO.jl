fd1ad02c45312af2d1d49beca647358e64c9a538
gyliu <liuguoyuan01@outlook.com>
2025-08-03 17:29:56 +0200

    add Hubbard
    1. add read and write for hubbard card
    2. add some utility functions for parsing pw inputs

--- src/common/const.jl
+++ src/common/const.jl
@@ -6,3 +6,20 @@ This is the default (Physical constants, SI (NIST 2018)) value in QE
 `Modules/constants.f90`.
 """
 const Bohr_QE::Float64 = 0.529177210903
+
+"""
+pw.x input keywords
+"""
+const PW_KEYWORDS = Set([
+    "atomic_species",
+    "atomic_positions",
+    "k_points",
+    "additional_k_points",
+    "cell_parameters",
+    "constraints",
+    "occupations",
+    "atomic_velocities",
+    "atomic_forces",
+    "solvents",
+    "hubbard",
+])
